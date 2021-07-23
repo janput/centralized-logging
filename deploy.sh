f01e1e392512375a16176c7129d8beea3c01d56e
Gilles Stragier <gilles.stragier@onem.be>
2021-07-23 16:41:30 +0200

    This first version might be working :)

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ helm repo add elastic https://helm.elastic.co
 helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update
 
-helm upgrade --kube-context $1 -f elasticsearch-values.yml --create-namespace -n centralized-logging -i --wait --timeout 120s elasticsearch elastic/elasticsearch
+helm upgrade --kube-context $1 -f elasticsearch-values.yml --create-namespace -n centralized-logging -i --wait --timeout 240s elasticsearch elastic/elasticsearch
 helm upgrade --kube-context $1 -f kibana-values.yml --create-namespace -n centralized-logging -i --wait --timeout 120s kibana elastic/kibana
 helm upgrade --kube-context $1 -f fluent-bit-values.yml --create-namespace -n centralized-logging -i --wait --timeout 20s fluent-bit fluent/fluent-bit
 
