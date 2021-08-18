aa7e70f8d8810c4dc468214265d5f967063635ca
PUT Jan <jan.put@rva.be>
2021-08-18 14:29:10 +0200

    added apm-server

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,4 @@ helm repo update
 helm upgrade --kube-context $1 -f elasticsearch-values.yml --create-namespace -n centralized-logging -i --wait --timeout 240s elasticsearch elastic/elasticsearch
 helm upgrade --kube-context $1 -f kibana-values.yml --create-namespace -n centralized-logging -i --wait --timeout 240s kibana elastic/kibana
 helm upgrade --kube-context $1 -f fluent-bit-values.yml --create-namespace -n centralized-logging -i --wait --timeout 20s fluent-bit fluent/fluent-bit
-
-
+helm upgrade --kube-context $1 -f apm-values.yaml --create-namespace -n centralized-logging -i --wait --timeout 20s apm-server elastic/apm-server
