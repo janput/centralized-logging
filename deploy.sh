16bd74b38db507eda779fda0029b471912c2c9b3
PUT Jan <jan.put@rva.be>
2021-08-18 15:05:48 +0200

    added metricbeat

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,4 @@ helm upgrade --kube-context $1 -f elasticsearch-values.yml --create-namespace -n
 helm upgrade --kube-context $1 -f kibana-values.yml --create-namespace -n centralized-logging -i --wait --timeout 240s kibana elastic/kibana
 helm upgrade --kube-context $1 -f fluent-bit-values.yml --create-namespace -n centralized-logging -i --wait --timeout 20s fluent-bit fluent/fluent-bit
 helm upgrade --kube-context $1 -f apm-values.yaml --create-namespace -n centralized-logging -i --wait --timeout 20s apm-server elastic/apm-server
+helm upgrade --kube-context $1 --create-namespace -n centralized-logging -i --wait --timeout 20s metricbeat elastic/metricbeat
