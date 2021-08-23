8b3d003fdd4e3f8d358985579a4f206132b2acd6
PUT Jan <jan.put@rva.be>
2021-08-23 12:02:12 +0200

    fix typo in filename

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ helm upgrade --kube-context $1 -f kibana-values.yml --create-namespace -n centra
 helm upgrade --kube-context $1 -f fluent-bit-values.yml --create-namespace -n centralized-logging -i --wait --timeout 20s fluent-bit fluent/fluent-bit
 helm upgrade --kube-context $1 -f apm-values.yaml --create-namespace -n centralized-logging -i --wait --timeout 240s apm-server elastic/apm-server
 helm upgrade --kube-context $1 -f metricbeat-values.yml -i -n kube-system metricbeat elastic/metricbeat
-helm upgrade --kube-context $1 -f elastichq-values.yml -i -n kube-system elastichq onemrva/elastichq
+helm upgrade --kube-context $1 -f elastichq-values.yaml -i -n kube-system elastichq onemrva/elastichq
